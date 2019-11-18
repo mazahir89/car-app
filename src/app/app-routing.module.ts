23df94d6b138d40d1e328b7b9932ffb8e1c1d2c5
Mazahir Hashimli <mazahir89@gmail.com>
2019-11-18 10:46:20 +0100

    added routeing for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarsComponent } from './cars/cars.component';
+import { ReportComponent } from './report/report.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'cars', component: CarsComponent},
+  {path: 'reports', component: ReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
